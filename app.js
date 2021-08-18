665d5679e598aa6fc67d648d4f768e12e56a53b2
Tony Vazgar <tonyvazgar@Tonys-MacBook-Pro.local>
2021-08-18 12:37:58 -0500

    casi listo el front de registrar

--- app.js
+++ app.js
@@ -12,3 +12,11 @@ app.listen(port, ()=>{
 app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname, '/views/home.html'));
 });
+
+app.get('/register', (req,res)=>{
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+});
